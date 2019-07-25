f4e1d4cfba2feec84d641cf33ad76d1ef42b3ee5
jangholi <marjan.jangholi@yahoo.com>
2019-07-25 14:50:15 +0430

    initial project

---
+++ js/public.js
@@ -0,0 +1,20 @@
+// $('#aboutMenu').click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#about").offset().top
+//     }, 2000);
+// })
+// $('#projectsMenu').click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#projects").offset().top
+//     }, 2000);
+// })
+// $('#contactMenu').click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#contactUs").offset().top
+//     }, 2000);
+// })
+// $('#gotoContact').click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#contactUs").offset().top
+//     }, 2000);
+// })
